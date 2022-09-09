3d77f163d89470ffbac2b6ffa88a2030aafa9fa1
MaxOvervoorde73 <max.overleeuw@gmail.com>
2022-09-09 14:21:07 +0200

    Updates and completed Assignment 2

--- ass02/Core/Src/main.c
+++ ass02/Core/Src/main.c
@@ -2,14 +2,14 @@
 
 #include <stdio.h>
 
-extern int test(int a, int b);
+extern int multiply(int a, int b);
 
 int main(void)
 {
 	extern void initialise_monitor_handles(void);
 	initialise_monitor_handles();
 
-	int a = test(3, 5);
+	int a = multiply(1000000, 54651564);
     printf("Result of test(3, 5) = %d\n", a);
     return 0;
 }
--- ass02/Core/Src/test.s
+++ ass02/Core/Src/test.s
@@ -1,9 +1,16 @@
 	.cpu cortex-m4
 	.thumb
 	.syntax unified
-	.globl test
+	.globl multiply
 	.text
 	.thumb_func
-test:
-	ADD.N	R0, R0, R1
-	BX.N	LR
+multiply:
+	MOVS.N R2, #0				// making variable for return value, R2 = m, R3 = i
+	MOVS.N R3, #0
+loop:
+	ADD.N	R2, R2, R1			// m = m + b
+	SUBS.N	R0, #1				// A = A - 1
+	CMP.N	R0, #0				// if A = 0
+	BNE.N 	loop				// als ie niet gelijk is aan 0, jump naar loop
+	MOVS.N 	R0, R2				// return M als return waarde
+	BX.N	LR					// weten we niet helemaal zeker
