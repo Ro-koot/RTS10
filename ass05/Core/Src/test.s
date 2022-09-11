ad0f687e3dce42beb447637a81a8fe911c32ee5b
MaxOvervoorde73 <max.overleeuw@gmail.com>
2022-09-11 11:12:41 +0200

    Untested assignment 2.5

--- ass05/Core/Src/test.s
+++ ass05/Core/Src/test.s
@@ -4,7 +4,7 @@
 	.globl multiply
 	.text
 	.thumb_func
-multiply:
+/*multiply:
 	MOVS.N R2, #0				// making variable for return value, R2 = m, R3 = i
 	MOVS.N R3, #0
 loop:
@@ -14,3 +14,28 @@ loop:
 	BNE.N 	loop				// als ie niet gelijk is aan 0, jump naar loop
 	MOVS.N 	R0, R2				// return M als return waarde
 	BX.N	LR					// weten we niet helemaal zeker
+*/
+
+multiply:
+	CMP.N R2, #0				//--> compare B met 0
+	BNE.N skip1					//--> Niet gelijk aan 0? skip naar skip1
+	MOVS.N R0, #0				//--> Wel gelijk aan 0? Move de waarde 0 naar R0 (return)
+	BX.N LR						//--> return naar eerdere laag code
+skip1:
+	CMP.N R2, #1				//--> compare B met 1
+	BNE.N skip2					//--> niet gelijk aan 1? skip naar skip2
+	ADDS.N R0, R0, R1			//--> wel gelijk aan 1? Tel a bij m op in return waarde
+	BX.N LR						//--> return naar eerdere laag code
+skip2:
+	MOVS.N R3, R2				//--> Maak R3 gelijk aan b
+	ANDS.N R3, R2				//--> bitmask zodat de eerste bit alleen gezien wordt, return op R3
+	CMP.N R3, #0				//--> is R3 gelijk aan 0?
+	BNE.N skip3					//--> Nee? skip3, Ja, ga door
+	LSLS.N R1, R1, #1			//--> Left shift A met 1 bit
+	LSRS.N R2, R2, #1			//--> right shift B met 1 bit
+	B.N multiply				//--> recurse de functie door te springen naar de functienaam opnieuw
+skip3:
+	ADDS.N R0, R0, R1			//--> Tel m bij a op
+	LSLS.N R1, R1, #1			//--> Left shift a met 1
+	LSRS.N R2, R2, #1			//--> right shift b met 1
+	B.N multiply
