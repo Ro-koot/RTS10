f0a204ec73bf7159a4971b837f0f7e98c554d336
MaxOvervoorde73 <max.overleeuw@gmail.com>
2022-09-11 15:15:11 +0200

    Implemented assignment 2.7
    Including:
    - power function
    - Recursion
    - Subroutine in subroutine by using LDR.N Rx, ="label"

--- ass07/Core/Src/main.c
+++ ass07/Core/Src/main.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 
+extern int power(int a, int b);
 extern int multiply(int a, int b);
 
 int main(void)
@@ -9,7 +10,7 @@ int main(void)
 	extern void initialise_monitor_handles(void);
 	initialise_monitor_handles();
 
-	int a = multiply(1000000, 54651564);
+	int a = power(2, 10);
     printf("Result of test(3, 5) = %d\n", a);
     return 0;
 }
--- ass07/Core/Src/test.s
+++ ass07/Core/Src/test.s
@@ -1,9 +1,10 @@
 	.cpu cortex-m4
 	.thumb
 	.syntax unified
-	.globl multiply
+	.globl power
 	.text
 	.thumb_func
+
 multiply:
 	MOVS.N R2, #0				// making variable for return value, R2 = m, R3 = i
 	MOVS.N R3, #0
@@ -14,3 +15,42 @@ loop:
 	BNE.N 	loop				// als ie niet gelijk is aan 0, jump naar loop
 	MOVS.N 	R0, R2				// return M als return waarde
 	BX.N	LR					// weten we niet helemaal zeker
+
+power:
+	PUSH.N {LR}
+	CMP.N 	R1, #0
+	MOVS.N	R2, #1
+	BNE.N	while_loop
+	MOVS.N 	R0, #1
+	BX.N	LR						// volgorde: R0 = n, R1 = m, R2 = p
+while_loop:
+	MOVS.N	R3, #1					// r3 = 1
+	ANDS.N	R3, R3, R1				// R1 is even of oneven
+	CMP.N	R3, #1					//--> immediately lose function of R3
+	BNE.N	skip
+	MOVS.N	R3, R1
+	MOVS.N	R1, R0
+	MOVS.N	R0, R2
+	MOVS.N	R2, R3					// volgorde: R0 = p, R1 = n, R2 = m, R3 = m
+	PUSH.N	{R1, R2, R3}			//--> push value of R1R2R3 on stack
+	LDR.N	R2, =multiply
+	BLX.N	R2
+	POP.N 	{R1, R2, R3}
+	MOVS.N 	R2, R0
+	MOVS.N 	R0, R1
+	MOVS.N 	R1, R3					// volgorde: R0 = n, R1 = m, R2 = p, R3 = m
+skip:
+	PUSH.N	{R1, R2}				// behoud waarde van m en p
+	MOVS.N	R1, R0					// R0 en R1 zijn n
+	LDR.N	R2, =multiply
+	BLX.N	R2
+	POP.N 	{R1, R2}				// volgorde: R0 = n, R1 = m, R2 = p, R3 = x
+	LSRS.N	R1, R1, #1
+	CMP.N	R1, #1
+	NOP.N
+	BNE.N	while_loop
+	MOVS.N 	R1, R2					// volgorde R0 = n, R1 = p, R2 = p, R3 = x
+	LDR.N	R2, =multiply
+	BLX.N	R2
+	POP.N	{PC}
+	BX.N 	LR
