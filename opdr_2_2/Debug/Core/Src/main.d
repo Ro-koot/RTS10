af1f9625a742040a04ab03923587bedf2d23ed2f
MaxOvervoorde73 <max.overleeuw@gmail.com>
2022-09-27 16:46:50 +0200

    Added 4.4 and previous

--- opdr_2_2/Debug/Core/Src/main.d
+++ opdr_2_2/Debug/Core/Src/main.d
@@ -1,17 +1,17 @@
 Core/Src/main.o: ../Core/Src/main.c \
- C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/Jaar\ 4/RTS10/Week\ 2/opdr_2_2/Core/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
- C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/Jaar\ 4/RTS10/Week\ 2/opdr_2_2/Core/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
- C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/Jaar\ 4/RTS10/Week\ 2/opdr_2_2/Core/CMSIS/Include/core_cm4.h \
- C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/Jaar\ 4/RTS10/Week\ 2/opdr_2_2/Core/CMSIS/Include/cmsis_version.h \
- C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/Jaar\ 4/RTS10/Week\ 2/opdr_2_2/Core/CMSIS/Include/cmsis_compiler.h \
- C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/Jaar\ 4/RTS10/Week\ 2/opdr_2_2/Core/CMSIS/Include/cmsis_gcc.h \
- C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/Jaar\ 4/RTS10/Week\ 2/opdr_2_2/Core/CMSIS/Include/mpu_armv7.h \
- C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/Jaar\ 4/RTS10/Week\ 2/opdr_2_2/Core/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
-C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/Jaar\ 4/RTS10/Week\ 2/opdr_2_2/Core/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
-C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/Jaar\ 4/RTS10/Week\ 2/opdr_2_2/Core/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
-C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/Jaar\ 4/RTS10/Week\ 2/opdr_2_2/Core/CMSIS/Include/core_cm4.h:
-C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/Jaar\ 4/RTS10/Week\ 2/opdr_2_2/Core/CMSIS/Include/cmsis_version.h:
-C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/Jaar\ 4/RTS10/Week\ 2/opdr_2_2/Core/CMSIS/Include/cmsis_compiler.h:
-C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/Jaar\ 4/RTS10/Week\ 2/opdr_2_2/Core/CMSIS/Include/cmsis_gcc.h:
-C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/Jaar\ 4/RTS10/Week\ 2/opdr_2_2/Core/CMSIS/Include/mpu_armv7.h:
-C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/Jaar\ 4/RTS10/Week\ 2/opdr_2_2/Core/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+ C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/github/ETS10/1.5/RTS10/opdr_2_2/Core/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/github/ETS10/1.5/RTS10/opdr_2_2/Core/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/github/ETS10/1.5/RTS10/opdr_2_2/Core/CMSIS/Include/core_cm4.h \
+ C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/github/ETS10/1.5/RTS10/opdr_2_2/Core/CMSIS/Include/cmsis_version.h \
+ C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/github/ETS10/1.5/RTS10/opdr_2_2/Core/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/github/ETS10/1.5/RTS10/opdr_2_2/Core/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/github/ETS10/1.5/RTS10/opdr_2_2/Core/CMSIS/Include/mpu_armv7.h \
+ C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/github/ETS10/1.5/RTS10/opdr_2_2/Core/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/github/ETS10/1.5/RTS10/opdr_2_2/Core/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/github/ETS10/1.5/RTS10/opdr_2_2/Core/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/github/ETS10/1.5/RTS10/opdr_2_2/Core/CMSIS/Include/core_cm4.h:
+C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/github/ETS10/1.5/RTS10/opdr_2_2/Core/CMSIS/Include/cmsis_version.h:
+C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/github/ETS10/1.5/RTS10/opdr_2_2/Core/CMSIS/Include/cmsis_compiler.h:
+C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/github/ETS10/1.5/RTS10/opdr_2_2/Core/CMSIS/Include/cmsis_gcc.h:
+C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/github/ETS10/1.5/RTS10/opdr_2_2/Core/CMSIS/Include/mpu_armv7.h:
+C:/Users/Max\ Overvoorde/Documents/School/Hogeschool\ Rotterdam/github/ETS10/1.5/RTS10/opdr_2_2/Core/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
